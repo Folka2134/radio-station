c888fd30ac64eb156c09e867455ef66df1273c2a
Folka2134 <folka2134@gmail.com>
2023-04-19 16:03:41 +0100

    built container for player

--- src/App.jsx
+++ src/App.jsx
@@ -1,17 +1,29 @@
 import "./App.css";
+import logo from "./assets/fcrlive.webp";
 
 function App() {
   return (
     <div className="App bg-orange-400 min-h-screen">
       <div className="mx-6 md:mx-24">
-        <header className="h-32 flex justify-between">
-          <div>logo</div>
-          <div className="bg-black text-white w-3/4">banner</div>
+        <header className="h-72 flex justify-between pt-3 mb-3">
+          <div className="flex justify-center">
+            <img className="h-full rounded-full" src={logo} alt="logo" />
+          </div>
+          <div className="bg-black text-white w-3/4 rounded-r-3xl">banner</div>
         </header>
-        <nav className="bg-green-200 rounded-t-lg">
+        <div className="flex sticky h-32 bg-amber-700 rounded-lg mb-1">
+          <div className="bg-gray-200 h-full w-full">
+            <div>currently playing</div>
+            <button>listen live ^^</button>
+          </div>
+          <div className="bg-gray-600 h-full w-full">
+            <h2>Request a song!</h2>
+          </div>
+        </div>
+        <nav className="bg-green-200 sticky rounded-t-lg">
           <ul className="flex">
             <li className="text-xl p-5 cursor-pointer hover:bg-green-100 rounded-tl-lg">
-              Home
+              Home^
             </li>
             <li className="text-xl p-5">Radio</li>
             <li className="text-xl p-5">News</li>
