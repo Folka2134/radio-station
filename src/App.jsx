9aee1585c6446c7ddf519b28940ef1dfe9862695
Folka2134 <folka2134@gmail.com>
2023-04-19 19:43:16 +0100

    created Homepage

--- src/App.jsx
+++ src/App.jsx
@@ -2,17 +2,11 @@ import "./App.css";
 import logo from "./assets/fcrlive.webp";
 import logoGofundme from "./assets/gofundme-blog-image.webp";
 import background from "./assets/plymouth.webp";
-import lottery from "./assets/lottery.webp";
-import radioAcademy from "./assets/radio_academy.webp";
-import pafc from "./assets/pafc.webp";
-import fund from "./assets/fund.webp";
-import clarion from "./assets/clarion.webp";
-import qrcode from "./assets/qrcode.webp";
-// import studio from "./assets/studio.webp";
 
 import { BsFacebook, BsTwitter } from "react-icons/bs";
 import { RxHamburgerMenu } from "react-icons/rx";
 import NewsList from "./components/NewsList";
+import Homepage from "./components/Homepage";
 
 function App() {
   return (
@@ -32,6 +26,7 @@ function App() {
           </div>
         </header>
         <div className="sticky top-5">
+          {/* PLAYER */}
           <div className="flex h-32 bg-black bg-opacity-80 rounded-xl mb-1 text-white border-2 border-white border-opacity-50 p-2">
             <div className="h-full w-full">
               <div>currently playing</div>
@@ -41,6 +36,7 @@ function App() {
               <h2>Request a song!</h2>
             </div>
           </div>
+          {/* NAV */}
           <nav className="flex justify-end md:justify-between md:bg-black md:bg-opacity-70 text-gray-300 md:border-2 border-black border-opacity-40 rounded mb-1">
             <ul className="hidden md:flex">
               <li className="text-xl p-5 cursor-pointer hover:text-white active:bg-white active:text-black transition-all duration-200 rounded-tl-lg">
@@ -64,52 +60,7 @@ function App() {
             </div>
           </nav>
         </div>
-        <main className="bg-[#1A3175] bg-opacity-60 border-2 border-black border-opacity-40 text-white text-2xl p-6">
-          <h1 className="text-5xl md:text-6xl text-center font-semibold">
-            Ferndale Community Radio Plymouth
-          </h1>
-          <div className="mt-2 text-center">
-            <h2 className="text-lg">
-              'The very best internet radio station from FCTG'
-            </h2>
-            <p className="text-[1rem]">
-              Based in the port city of Plymouth - Devon - UK.
-            </p>
-          </div>
-          <div className="mt-6 grid grid-cols-3 grid-rows-2 justify-items-center 2xl:px-52">
-            <img
-              className="w-[80%] xl:w-[40%]  aspect-square object-contain"
-              src={lottery}
-              alt="lottery"
-            />
-            <img
-              className="w-[80%] xl:w-[40%] aspect-square object-contain"
-              src={radioAcademy}
-              alt="radioAcademy"
-            />
-            <img
-              className="w-[80%] xl:w-[40%] aspect-square object-contain "
-              src={pafc}
-              alt="pafc"
-            />
-            <img
-              className="w-[80%] xl:w-[40%] aspect-square object-contain"
-              src={fund}
-              alt="fund"
-            />
-            <img
-              className="w-[80%] xl:w-[40%] aspect-square object-contain"
-              src={clarion}
-              alt="clarion"
-            />
-            <img
-              className="w-[80%] xl:w-[40%] aspect-square object-contain"
-              src={qrcode}
-              alt="qrcode"
-            />
-          </div>
-          {/* <NewsList /> */}
-        </main>
+        <Homepage />
         <footer className="bg-black bg-opacity-70 rounded-b-xl text-gray-300 border-2 border-black border-opacity-40">
           <div className="flex justify-between pl-5 pr-0 py-2 md:px-24">
             <div className="flex md:flex-row flex-col items-center justify-center">
---
+++ src/components/Homepage.jsx
@@ -0,0 +1,60 @@
+import React from "react";
+import lottery from "../assets/lottery.webp";
+import radioAcademy from "../assets/radio_academy.webp";
+import pafc from "../assets/pafc.webp";
+import fund from "../assets/fund.webp";
+import clarion from "../assets/clarion.webp";
+import qrcode from "../assets/qrcode.webp";
+
+const Homepage = () => {
+  return (
+    <main className="bg-[#1A3175] bg-opacity-60 border-2 border-black border-opacity-40 text-white text-2xl p-6">
+      <h1 className="text-5xl md:text-6xl text-center font-semibold">
+        Ferndale Community Radio Plymouth
+      </h1>
+      <div className="mt-2 text-center">
+        <h2 className="text-lg">
+          'The very best internet radio station from FCTG'
+        </h2>
+        <p className="text-[1rem]">
+          Based in the port city of Plymouth - Devon - UK.
+        </p>
+      </div>
+      <div className="mt-6 grid grid-cols-3 grid-rows-2 justify-items-center 2xl:px-52">
+        <img
+          className="w-[80%] xl:w-[40%]  aspect-square object-contain"
+          src={lottery}
+          alt="lottery"
+        />
+        <img
+          className="w-[80%] xl:w-[40%] aspect-square object-contain"
+          src={radioAcademy}
+          alt="radioAcademy"
+        />
+        <img
+          className="w-[80%] xl:w-[40%] aspect-square object-contain "
+          src={pafc}
+          alt="pafc"
+        />
+        <img
+          className="w-[80%] xl:w-[40%] aspect-square object-contain"
+          src={fund}
+          alt="fund"
+        />
+        <img
+          className="w-[80%] xl:w-[40%] aspect-square object-contain"
+          src={clarion}
+          alt="clarion"
+        />
+        <img
+          className="w-[80%] xl:w-[40%] aspect-square object-contain"
+          src={qrcode}
+          alt="qrcode"
+        />
+      </div>
+      {/* <NewsList /> */}
+    </main>
+  );
+};
+
+export default Homepage;
