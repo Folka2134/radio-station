46ae9eba447dbd1681cdb4f99a958dda24daaead
Folka2134 <folka2134@gmail.com>
2023-04-20 11:37:14 +0100

    grabbed an array of presenters

---
+++ src/presenters.js
@@ -0,0 +1,144 @@
+const presenters = [
+  {
+    Amanda_Mansfield: {
+      name: "Amanda Mansfield",
+      image: "Amanda_Mansfield.webp",
+    },
+    Andrew_Hill: {
+      name: "Andrew Hill",
+      image: "Andrew_Hill.webp",
+    },
+    Andy_Howard: {
+      name: "Andy Howard",
+      image: "Andy_Howard.webp",
+    },
+    Andy_Valentine: {
+      name: "Andy Valentine",
+      image: "Andy_Valentine.webp",
+    },
+    Anita_Monk: {
+      name: "Anita Monk",
+      image: "Anita_Monk.webp",
+    },
+    "Becky_Lacey-Sawyer": {
+      name: "Becky Lacey-Sawyer",
+      image: "Becky_Lacey-Sawyer.webp",
+    },
+    Billy_Jay: {
+      name: "Billy Jay",
+      image: "Billy_Jay.webp",
+    },
+    Charlotte_Willis: {
+      name: "Charlotte Willis",
+      image: "Charlotte_Willis.webp",
+    },
+    Colin_Davey: {
+      name: "Colin Davey",
+      image: "Colin_Davey.webp",
+    },
+    Craig_Sullivan: {
+      name: "Craig Sullivan",
+      image: "Craig_Sullivan.webp",
+    },
+    David_Lees: {
+      name: "David Lees",
+      image: "David_Lees.webp",
+    },
+    Dinah_Greek: {
+      name: "Dinah Greek",
+      image: "Dinah_Greek.webp",
+    },
+    George_Bond: {
+      name: "George Bond",
+      image: "George_Bond.webp",
+    },
+    Gimpy_Jake: {
+      name: "Gimpy Jake",
+      image: "Gimpy_Jake.webp",
+    },
+    Hilary_Dunlop: {
+      name: "Hilary Dunlop",
+      image: "Hilary_Dunlop.webp",
+    },
+    Jade_Scott: {
+      name: "Jade Scott",
+      image: "Jade_Scott.webp",
+    },
+    Julian_Mildren: {
+      name: "Julian Mildren",
+      image: "Julian_Mildren.webp",
+    },
+    Michael_Tracey: {
+      name: "Michael Tracey",
+      image: "Michael_Tracey.webp",
+    },
+    Mike_Roberts: {
+      name: "Mike Roberts",
+      image: "Mike_Roberts.webp",
+    },
+    Mike_Snook: {
+      name: "Mike Snook",
+      image: "Mike_Snook.webp",
+    },
+    "Mimi-Dorothea_Orme": {
+      name: "Mimi-Dorothea Orme",
+      image: "Mimi-Dorothea_Orme.webp",
+    },
+    Nathassia: {
+      name: "Nathassia",
+      image: "Nathassia.webp",
+    },
+    Nicholas_Mark_Ross: {
+      name: "Nicholas Mark Ross",
+      image: "Nicholas_Mark_Ross.webp",
+    },
+    Nick_Taylor: {
+      name: "Nick Taylor",
+      image: "Nick_Taylor.webp",
+    },
+    Nikki_Nichols: {
+      name: "Nikki Nichols",
+      image: "Nikki_Nichols.webp",
+    },
+    Pete_Rattlebone: {
+      name: "Pete Rattlebone",
+      image: "Pete_Rattlebone.webp",
+    },
+    Phoenix_Walker: {
+      name: "Phoenix Walker",
+      image: "Phoenix_Walker.webp",
+    },
+    "Rick_O'Shay": {
+      name: "Rick O'Shay",
+      image: "Rick_O'Shay.webp",
+    },
+    Roger_Schulz: {
+      name: "Roger Schulz",
+      image: "Roger_Schulz.webp",
+    },
+    Rozanne_Griffith: {
+      name: "Rozanne Griffith",
+      image: "Rozanne_Griffith.webp",
+    },
+    Steve_Medlin: {
+      name: "Steve Medlin",
+      image: "Steve_Medlin.webp",
+    },
+    Tom_Podesta: {
+      name: "Tom Podesta",
+      image: "Tom_Podesta.webp",
+    },
+    Tracey_Ann_Burke: {
+      name: "Tracey Ann Burke",
+      image: "Tracey_Ann_Burke.webp",
+    },
+    "We.Are.Alien": {
+      name: "We.Are.Alien",
+      image: "We.Are.Alien.webp",
+    },
+    wilma_420: {
+      name: "wilma_420",
+      image: "wilma_420.webp",
+    },
+  },
+];
