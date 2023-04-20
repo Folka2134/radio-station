b654ee4d633ceb6c6b1934ce999c7586b6f47726
Folka2134 <folka2134@gmail.com>
2023-04-20 09:39:07 +0100

    moved data to homepage

--- src/components/Radiopage.jsx
+++ src/components/Radiopage.jsx
@@ -2,28 +2,22 @@ import React from "react";
 
 import studio from "../assets/studio.webp";
 
-const Homepage = () => {
+const Radiopage = () => {
   return (
     <main className="bg-[#1A3175] bg-opacity-60 border-2 border-black border-opacity-40 text-white text-2xl p-6">
-      <h1 className="text-5xl md:text-6xl text-center font-semibold">
+      <h1 className="text-4xl md:text-4xl text-center font-semibold">
         Ferndale Community Radio - Plymouth, Devon, UK.
       </h1>
-      <div className="mt-2 text-center">
-        <h2 className="text-lg">STUDIO NUMBER ONE</h2>
-        <p className="text-[1rem]">
-          Ferndale Community Radio, FCR for short, is an online based community
-          radio station operating out of the Ferndale Community Hub. FCR
-          provides information and entertainment to the local communities in and
-          around Plymouth, while also being open to the UK and other countries
-          around the world. We are now on air 24 hours a day, 7 days a week.
-          Download the App from Google Play or Apple for easier listening, also
-          available on Amazon Alexa.
-        </p>
+      <div className="flex flex-col mt-2 text-center">
+        <h2 className="text-lg font-bold">STUDIO NUMBER ONE</h2>
+        <div className="flex flex-col xl:flex-row">
+          <img className="" src={studio} alt="studio" />
+          <p className="text-[1rem]"></p>
+        </div>
       </div>
-
       {/* <NewsList /> */}
     </main>
   );
 };
 
-export default Homepage;
+export default Radiopage;
