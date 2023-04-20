48fca293768ad0030d6b7ffcd8be3a516457c97a
Folka2134 <folka2134@gmail.com>
2023-04-20 13:43:44 +0100

    fixed grid

--- src/components/Grid.jsx
+++ src/components/Grid.jsx
@@ -1,36 +1,22 @@
-import React, { useEffect, useRef } from "react";
+import React from "react";
+import { presenters } from "../presenters";
 
 import "../App.css";
 
 const Grid = () => {
-  const images = []; // Add the other images to this array
-  const gridRef = useRef(null);
-
-  useEffect(() => {
-    const grid = gridRef.current;
-    const images = grid.querySelectorAll(".image");
-    const observer = new IntersectionObserver((entries) => {
-      entries.forEach((entry) => {
-        if (entry.isIntersecting) {
-          entry.target.classList.add("fade-in");
-        }
-      });
-    });
-    images.forEach((image) => observer.observe(image));
-  }, []);
-
   return (
-    <div className="myGrid" ref={gridRef}>
-      {[...Array(6)].map((_, rowIndex) => (
-        <div className="row " key={rowIndex}>
-          {[...Array(6)].map((_, colIndex) => (
-            <img
-              className="image"
-              key={`${rowIndex}-${colIndex}`}
-              src={images[colIndex % images.length]}
-              alt="grid image"
-            />
-          ))}
+    <div className="grid grid-cols-3 md:grid-cols-6 2xl:grid-cols-8 gap-1 2xl:gap-0 ">
+      {presenters.map((presenter, keyId) => (
+        <div
+          key={keyId}
+          className="flex flex-col justify-center h-44 rounded-xl"
+        >
+          <img
+            className="image object-scale-down h-28 rounded-xl"
+            src={presenter.image}
+            alt="presenter"
+          />
+          <p className="text-xs whitespace-nowrap">{presenter.name}</p>
         </div>
       ))}
     </div>
