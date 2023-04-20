837118029de08058950d10690f9d2443b2917a85
Folka2134 <folka2134@gmail.com>
2023-04-20 09:38:01 +0100

    created route state + switch

--- src/App.jsx
+++ src/App.jsx
@@ -1,3 +1,5 @@
+import React, { useState } from "react";
+
 import "./App.css";
 import logo from "./assets/fcrlive.webp";
 import logoGofundme from "./assets/gofundme-blog-image.webp";
@@ -7,8 +9,22 @@ import { BsFacebook, BsTwitter } from "react-icons/bs";
 import { RxHamburgerMenu } from "react-icons/rx";
 import NewsList from "./components/NewsList";
 import Homepage from "./components/Homepage";
+import Radiopage from "./components/Radiopage";
+
+function renderSwitch(param) {
+  switch (param) {
+    case "home":
+      return <Homepage />;
+    case "radio":
+      return <Radiopage />;
+    default:
+      return <Homepage />;
+  }
+}
 
 function App() {
+  const [route, setRoute] = useState("home");
+
   return (
     <div className="App bg-[url('./assets/plymouth.webp')] bg-fixed bg-cover min-h-screen relative min-w-[350px]">
       <div className="absolute top-0 left-0 w-full h-full bg-black bg-opacity-50 z-0"></div>
@@ -39,15 +55,24 @@ function App() {
           {/* NAV */}
           <nav className="flex justify-end md:justify-between md:bg-black md:bg-opacity-70 text-gray-300 md:border-2 border-black border-opacity-40 rounded mb-1">
             <ul className="hidden md:flex">
-              <li className="text-xl p-5 cursor-pointer hover:text-white active:bg-white active:text-black transition-all duration-200 rounded-tl-lg">
+              <button
+                className="text-xl p-5 cursor-pointer hover:text-white active:bg-white active:text-black transition-all duration-200 rounded-tl-lg"
+                onClick={() => setRoute("home")}
+              >
                 Home
-              </li>
-              <li className="text-xl p-5 cursor-pointer hover:text-white active:bg-white active:text-black transition-all duration-200">
+              </button>
+              <button
+                className="text-xl p-5 cursor-pointer hover:text-white active:bg-white active:text-black transition-all duration-200"
+                onClick={() => setRoute("radio")}
+              >
                 Radio
-              </li>
-              <li className="text-xl p-5 cursor-pointer hover:text-white active:bg-white active:text-black transition-all duration-200">
+              </button>
+              <button
+                className="text-xl p-5 cursor-pointer hover:text-white active:bg-white active:text-black transition-all duration-200"
+                onClick={() => setRoute("news")}
+              >
                 News
-              </li>
+              </button>
               <li className="text-xl p-5 cursor-pointer hover:text-white active:bg-white active:text-black transition-all duration-200">
                 Contact
               </li>
@@ -60,7 +85,8 @@ function App() {
             </div>
           </nav>
         </div>
-        <Homepage />
+        {/* Render Main */}
+        {renderSwitch(route)}
         <footer className="bg-black bg-opacity-70 rounded-b-xl text-gray-300 border-2 border-black border-opacity-40">
           <div className="flex justify-between pl-5 pr-0 py-2 md:px-24">
             <div className="flex md:flex-row flex-col items-center justify-center">
