ad4dbc91d7352ef84ff85ae853c9587bd37e9a0c
Folka2134 <folka2134@gmail.com>
2023-04-21 11:09:55 +0100

    added new media queries

--- tailwind.config.js
+++ tailwind.config.js
@@ -5,7 +5,14 @@ export default {
     fontFamily: {
       roboto: ["Roboto", "sans-serif"],
     },
-    extend: {},
+    extend: {
+      screens: {
+        "3xl": { raw: "(min-width: 1600px)" },
+        "4xl": { raw: "(min-width: 1800px)" },
+        "5xl": { raw: "(min-width: 2000px)" },
+        // => @media (min-height: 800px) { ... }
+      },
+    },
   },
   plugins: [],
 };
