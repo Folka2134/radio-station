39b9ffada80f7785779345bb8f463c53a59c681a
Folka2134 <folka2134@gmail.com>
2023-04-27 22:14:49 +0100

    updated player location and style

--- src/App.jsx
+++ src/App.jsx
@@ -10,6 +10,7 @@ import { BsFacebook, BsTwitter } from "react-icons/bs";
 import { RxHamburgerMenu } from "react-icons/rx";
 import { BsFillMoonFill } from "react-icons/bs";
 import { BsFillSunFill } from "react-icons/bs";
+import { BsFillPlayFill } from "react-icons/bs";
 
 import "./App.css";
 
@@ -89,12 +90,6 @@ function App() {
               >
                 Radio
               </button>
-              {/* <button
-                className="text-xl p-5 cursor-pointer hover:text-white active:bg-white active:text-black transition-all duration-200"
-                onClick={() => setRoute("news")}
-              >
-                News
-              </button> */}
               <button
                 className="text-xl p-5 cursor-pointer hover:text-white transition-all duration-200"
                 onClick={() => setRoute("contact")}
@@ -127,27 +122,28 @@ function App() {
           <div className="border-white border-2 text-white ">Advert</div>
         </section> */}
           <div className="mx-6 lg:mx-12 xl:mx-24 2xl:mx-44 3xl:mx-52 4xl:mx-72 5xl:mx-96 z-10 relative">
-            <header className="flex flex-col md:flex-row justify-center pt-3 mb-3">
-              <div className="flex justify-center md:mr-5 mb-2 w-full">
+            <header className="flex flex-col md:flex-row justify-center items-center pt-3 mb-3">
+              <div className="flex justify-center md:mr-5 mb-2 ">
                 <img
                   className="max-h-60 md:h-full rounded-full float-right"
                   src={logo}
                   alt="logo"
                 />
               </div>
-            </header>
-            <div className="sticky top-16">
               {/* PLAYER */}
-              {/* <div className="mx-6 lg:mx-12 xl:mx-24 2xl:mx-44 3xl:mx-52 4xl:mx-72 5xl:mx-60 flex h-32 bg-[#242424] bg-opacity-90 rounded-xl mb-1 text-white border-2 border-black border-opacity-50 p-2">
-              <div className="h-full w-full">
-                <div>currently playing</div>
-                <button>listen live ^^</button>
+              <div className="flex flex-col h-60 w-60 text-center bg-[#242424] rounded-xl text-white p-2 mt-5 md:mt-0 mx-6">
+                <div className="h-full w-full border-2 border-white rounded-xl mb-2">
+                  <div>Listen Live</div>
+                  <button className="mt-14">
+                    <BsFillPlayFill size={35} />
+                  </button>
+                </div>
+                <div className="bg-white rounded-xl text-black">
+                  <h2>Make a request!</h2>
+                </div>
               </div>
-              <div className="h-full w-full">
-                <h2>Request a song!</h2>
-              </div>
-            </div> */}
-            </div>
+            </header>
+            <div className="sticky top-16"></div>
             <div className="flex flex-col mb-10">{renderSwitch(route)}</div>
           </div>
           <footer className="flex flex-col md:flex-row justify-center items-center py-4 bg-[#1D1D1D] text-gray-300 mt-auto">
